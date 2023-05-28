ad44d9aa4a2abd4307483a2765219c70ec9c76af
HuyHoang <nhhoang.0303@gmail.com>
2023-05-28 20:58:28 +0700

    sửa file pubspec

--- lib/screens/wellcome_screen.dart
+++ lib/screens/wellcome_screen.dart
@@ -1,43 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/screens/home_screen.dart';
 import 'package:flutter_application_2/screens/signup_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 
-class WellcomeScreen extends StatelessWidget {
+class WelcomeScreen extends StatelessWidget {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
   FirebaseAuth _auth = FirebaseAuth.instance;
 
-  Future<void> signIn() async {
+  DatabaseReference databaseReference = FirebaseDatabase.instance.reference();
+
+  Future<void> signIn(BuildContext context) async {
     try {
       UserCredential userCredential = await _auth.signInWithEmailAndPassword(
         email: emailController.text,
         password: passwordController.text,
       );
 
-      // Kết nối với Realtime Database
-      // ignore: deprecated_member_use
-      DatabaseReference databaseReference =
-          FirebaseDatabase.instance.reference();
-
-      // Lấy dữ liệu người dùng từ Realtime Database
-      DatabaseEvent dataSnapshot = await databaseReference
+      DataSnapshot dataSnapshot = await databaseReference
           .child('users')
           .child(userCredential.user!.uid)
           .once();
-      Map<dynamic, dynamic>? userData =
-          dataSnapshot.snapshot.value as Map<dynamic, dynamic>?;
 
-      // Sử dụng dữ liệu người dùng
+      Map<dynamic, dynamic> userData =
+          dataSnapshot.value as Map<dynamic, dynamic>;
+
       if (userData != null) {
         String username = userData['username'];
         String email = userData['email'];
-
-        // Tiếp tục xử lý hoặc chuyển hướng đến màn hình khác
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomeScreen()),
+        );
       } else {
         print('Không tìm thấy dữ liệu người dùng.');
       }
@@ -47,6 +46,8 @@ class WellcomeScreen extends StatelessWidget {
       } else if (e.code == 'wrong-password') {
         print('Mật khẩu không chính xác.');
       }
+    } catch (e) {
+      print('Đã xảy ra lỗi: $e');
     }
   }
 
@@ -56,12 +57,16 @@ class WellcomeScreen extends StatelessWidget {
       child: Container(
         padding: EdgeInsets.only(top: 100, bottom: 40),
         decoration: BoxDecoration(
-            color: Colors.black,
-            image: DecorationImage(
-              image: AssetImage("images/hinhnen.jpg"),
-              fit: BoxFit.cover,
-              opacity: 0.6,
-            )),
+          color: Colors.black,
+          image: DecorationImage(
+            image: AssetImage("images/hinhnen.jpg"),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+              Colors.black.withOpacity(0.6),
+              BlendMode.dstATop,
+            ),
+          ),
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
@@ -125,6 +130,7 @@ class WellcomeScreen extends StatelessWidget {
                   ),
                   child: TextFormField(
                     controller: passwordController,
+                    obscureText: true,
                     style: TextStyle(
                       fontSize: 16,
                       color: Colors.black87,
@@ -143,92 +149,64 @@ class WellcomeScreen extends StatelessWidget {
                     ),
                   ),
                 ),
-              ],
-            ),
-            Column(
-              children: [
-                const Text.rich(
-                  TextSpan(
-                      text: "Bạn quên ",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w500,
-                        letterSpacing: 1,
+                Container(
+                  height: 60,
+                  margin: EdgeInsets.only(bottom: 20, left: 20, right: 20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.orange,
+                        Colors.deepOrange,
+                      ],
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.3),
+                        offset: Offset(0, 3),
+                        blurRadius: 6,
                       ),
-                      children: [
-                        TextSpan(
-                          text: "mật khẩu?",
-                          style: TextStyle(
-                            color: Colors.lightBlueAccent,
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500,
-                            letterSpacing: 1,
-                          ),
-                        )
-                      ]),
-                ),
-                SizedBox(
-                  height: 40,
-                ),
-                InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => SignUpScreen()));
-                  },
-                  child: Ink(
-                    child: Container(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 15, horizontal: 50),
-                      decoration: BoxDecoration(
-                          color: Color(0xFFE57734),
-                          borderRadius: BorderRadius.circular(10)),
-                      child: Text(
-                        "Đăng ký tài khoản",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 22,
-                          letterSpacing: 1,
-                        ),
+                    ],
+                  ),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.transparent,
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    onPressed: () {
+                      signIn(context);
+                    },
+                    child: Text(
+                      "Đăng Nhập",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
                       ),
                     ),
                   ),
                 ),
-                SizedBox(
-                  height: 80,
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SignUpScreen()),
+                    );
+                  },
+                  child: Text(
+                    "Đăng Ký",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
-                ElevatedButton(
-                  onPressed: signIn,
-                  child: Text('Đăng nhập'),
-                ),
-                //inkwell la gi?
-                // InkWell(
-                //   onTap: () {
-                //     Navigator.push(context,
-                //         MaterialPageRoute(builder: (context) => HomeScreen()));
-                //   },
-                //   child: Ink(
-                //     child: Container(
-                //       padding:
-                //           EdgeInsets.symmetric(vertical: 15, horizontal: 50),
-                //       decoration: BoxDecoration(
-                //           color: Color(0xFFE57734),
-                //           borderRadius: BorderRadius.circular(10)),
-                //       child: Text(
-                //         "Đăng nhập",
-                //         style: TextStyle(
-                //           color: Colors.white,
-                //           fontSize: 22,
-                //           letterSpacing: 1,
-                //         ),
-                //       ),
-                //     ),
-                //   ),
-                // )
               ],
-            )
+            ),
           ],
         ),
       ),
