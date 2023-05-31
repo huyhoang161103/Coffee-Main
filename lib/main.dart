65a3600a1e3fcc6fb4cc0558644aa876d2e843ea
HuyHoang <nhhoang.0303@gmail.com>
2023-05-31 13:45:45 +0700

    login database

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_2/firebase_options.dart';
-import 'package:flutter_application_2/screens/home_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter_application_2/screens/wellcome_screen.dart';
+import 'package:flutter_application_2/screens/sign_in.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: FirebaseOptions(
-      apiKey: 'AIzaSyD6qO83EaSUMCd1uVNhBJXNT1mhekAxfdM',
-      projectId: 'coffeeapp-project',
-      databaseURL: 'https://coffeeapp-project-default-rtdb.firebaseio.com',
-      messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-      appId: '1:486245911561:android:85c3ed190de1a5fe5fbcd8',
+      appId: '1:165852388585:android:c6d7bad678f67d45b1e478',
+      apiKey: 'AIzaSyD3X2WdwVTYoyMKC8xxOjEl0FDH1wTJ4CI',
+      projectId: 'app-coffee-f13ab',
+      messagingSenderId: 'YOUR_SENDER_ID',
     ),
   );
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,7 +24,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Color(0xFF212325),
       ),
       debugShowCheckedModeBanner: false,
-      home: WelcomeScreen(),
+      home: SignIn(),
     );
   }
 }
